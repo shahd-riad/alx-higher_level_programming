18d6b8f721d04925f50c84ec8ae0e5c559fc1317
shahd-riad <shahd.riad2344@gmail.com>
2024-01-29 23:18:00 +0200

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 
 if __name__ == "__main__":
     import sys
@@ -7,8 +6,8 @@ if __name__ == "__main__":
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument:")
+        print("1 argument")
     else:
-         print("{} arguments:".format(count))
+        print("{} arguments:".format(count))
     for i in range(count):
-         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
