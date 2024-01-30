4dfcfcce249787b663d37d4de9db5783757e16cb
shahd-riad <shahd.riad2344@gmail.com>
2024-01-30 05:00:22 +0200

    aaaaa

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
-
- #!/usr/bin/python3
- def multiple_returns(sentence):
-     length = len(sentence)
-     if sentence == 0:
-         firstL = 'None'
-     else:
-         firstL = sentence[0]
-     return length, firstL
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence == 0:
+        firstL = 'None'
+    else:
+        firstL = sentence[0]
+    return length, firstL
